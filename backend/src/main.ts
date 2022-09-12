de059f4ca3017665048b9081a092dcfa529a5501
Rania Fahmy <rania.fahmy222@gmail.com>
2022-09-12 14:50:33 +0200

    smoke tests and clean ups

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -10,6 +10,8 @@ import { TransformInterceptor } from './modules/common/interceptors/TransformInt
 import * as express from 'express';
 import { ErrorFilter } from './modules/errors/error.filter';
 
+console.log('Hey there, this is the development branch. Only CI takes place here.')
+
 async function bootstrap() {
   const logger = new AppLogger();
   logger.info(`NodeJs Version ${process.version}`);
